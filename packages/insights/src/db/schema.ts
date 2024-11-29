7b63fd9bd1ed765eadb3b67988e618da2aeae84c
gioboa <giorgiob.boa@gmail.com>
2024-11-29 15:01:17 +0100

    chore: linter 🧽

--- packages/insights/src/db/schema.ts
+++ packages/insights/src/db/schema.ts
@@ -73,18 +73,12 @@ export const manifestTable = sqliteTable(
     timestamp: integer('timestamp', { mode: 'timestamp_ms' }).notNull(),
   },
   (table) => ({
-    publicApiKeyHashIndex: index('idx_manifests_apiKey_hash').on(
-      table.hash,
-      table.publicApiKey
-    ),
+    publicApiKeyHashIndex: index('idx_manifests_apiKey_hash').on(table.hash, table.publicApiKey),
     publicApiKeyHashIndex_2: index('idx_manifests_apiKey_hash_2').on(
       table.publicApiKey,
       table.hash
     ),
-    apiTimestamp: index('idx_manifest_api_timestamp').on(
-      table.publicApiKey,
-      table.timestamp
-    ),
+    apiTimestamp: index('idx_manifest_api_timestamp').on(table.publicApiKey, table.timestamp),
     publicApiKeyIndex: index('idx_manifests_public_apiKey').on(table.publicApiKey),
     hashIndex: index('idx_manifests_hash').on(table.hash),
   })
